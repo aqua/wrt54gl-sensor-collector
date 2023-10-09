88c96e39c2f69389a6b70f1fdc50b3da97ee556d
Devin Carraway <git@devin.com>
2023-10-08 21:29:22 -0700

    Trim down logging, add basic connection status metrics

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"net/http"
 	"regexp"
@@ -33,6 +34,16 @@ var (
 		Name:      "relative_humidity_percent",
 		Help:      "Relative humidity sampled from a single sensor, in percent",
 	}, []string{"id", "device", "model"})
+	connectionAttempts = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "sensors",
+		Name:      "connection_attempts",
+		Help:      "Attempts to connect ot WRT54GL",
+	})
+	connectionErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "sensors",
+		Name:      "connection_errors",
+		Help:      "Failures to connect ot WRT54GL",
+	})
 	samplesReceived = prometheus.NewCounter(prometheus.CounterOpts{
 		Namespace: "sensors",
 		Name:      "samples_received",
@@ -69,7 +80,9 @@ func recordDS18x20(kind, ID, model, value string) {
 		log.Printf("Error parsing sample value %q from device %q: %v", value, ID, err)
 		return
 	}
-	fv = (fv - 32.) * 5. / 9.
+	// Convert back to celsius and round to nearest 0.1 degrees; the DS18S20 and
+	// DS18B20 were both precise to within ±0.5°.
+	fv = math.Round(10*(fv-32.)*5./9.) / 10.
 	switch kind {
 	case "temp":
 		temperatureGauges.With(prometheus.Labels{
@@ -94,8 +107,12 @@ func recordDHT22(kind, model, v1, v2 string) {
 		"model":  strings.ToLower(model),
 	}
 	humidityGauges.With(labels).Set(hv)
+
 	tv, err := strconv.ParseFloat(v2, 64)
-	tv = (tv - 32.) * 5. / 9.
+	// For some reason past-me had this output in fahrenheit, and now can't
+	// reflash to fix it; convert it back.  Round to 0.1 degrees, since the
+	// DHT22 has a precision of ±0.5°C and reporting more is pointless.
+	tv = math.Round(10*(tv-32.)*5./9.) / 10.
 	if err != nil {
 		log.Printf("Error parsing sample value 2 %q from device %q: %v", v2, model, err)
 		return
@@ -104,26 +121,38 @@ func recordDHT22(kind, model, v1, v2 string) {
 }
 
 func redial() {
+	connectNum := 0
 	for {
+		seen := map[string]bool{}
+		connectionAttempts.Inc()
 		conn, err := net.DialTimeout("tcp", *connect, *connectTimeout)
 		if err != nil {
 			log.Printf("Error connecting to %s: %v", *connect, err)
+			connectionErrors.Inc()
 			time.Sleep(5 * time.Second)
 			continue
 		}
+		connectNum++
+		log.Printf("Connected to %s (connection %d)", *connect, connectNum)
 		scanner := bufio.NewScanner(conn)
 		for scanner.Scan() {
 			t := scanner.Text()
-			log.Printf("read: %s", t)
 			bytesReceived.Add(float64(len(t) + 1))
 			if m := ds18x20SampleRE.FindStringSubmatch(t); m != nil {
 				samplesReceived.Inc()
+				if !seen[m[2]] {
+					seen[m[2]] = true
+					log.Printf("Got first sample from %s in connection %d", m[2], connectNum)
+				}
 				recordDS18x20(m[1], m[2], m[3], m[4])
 			} else if m := dht22SampleRE.FindStringSubmatch(t); m != nil {
 				samplesReceived.Inc()
+				if !seen[m[2]] {
+					seen[m[2]] = true
+					log.Printf("Got first sample from %s in connection %d", m[2], connectNum)
+				}
 				recordDHT22(m[1], m[2], m[3], m[4])
 			}
-
 		}
 		if err := scanner.Err(); err != nil {
 			log.Printf("Read failed from %s: %v", *connect, err)
@@ -135,7 +164,8 @@ func redial() {
 
 func main() {
 	flag.Parse()
-	prometheus.MustRegister(samplesReceived, bytesReceived, temperatureGauges)
+	prometheus.MustRegister(connectionAttempts, connectionErrors,
+		samplesReceived, bytesReceived, temperatureGauges, humidityGauges)
 	go redial()
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*listen, nil))
